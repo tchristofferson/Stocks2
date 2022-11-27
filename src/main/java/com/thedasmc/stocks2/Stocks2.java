cefb9be8bd752dc9ec32087c50a47679b86f8ccd
tchristofferson <longleadtrc@gmail.com>
2022-11-27 14:10:29 -0600

    Initial commit

---
+++ src/main/java/com/thedasmc/stocks2/Stocks2.java
@@ -0,0 +1,17 @@
+package com.thedasmc.stocks2;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Stocks2 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
